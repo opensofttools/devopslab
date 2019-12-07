45152aa5034dec3b401b786a8487473e2dc194db
kirin <kirin_13@163.com>
2019-12-07 14:43:49 +0800

    add new line

--- src/app/hello.go
+++ src/app/hello.go
@@ -15,6 +15,7 @@ func say(word string) string {
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Say something!")
 	fmt.Fprintf(w, "%s\n", say("BLUE-IBM!!!"))
+	fmt.Fprintf(w, "%s\n", say("GREN-IBM!!!"))
 }
 
 func main() {

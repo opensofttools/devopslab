c0bfd3c4f7d2dbbde0df6f8d12335d0ae6d71738
kirin <kirin_13@163.com>
2019-12-07 13:37:13 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=kirin_13
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing07
+export KUBECONFIG=/tmp/ic/AA/.bluemix/plugins/container-service/clusters/tektonknativebeijing07/kube-config-tok04-tektonknativebeijing07.yml
 
-export NAMESPACE=
+export NAMESPACE=kirin
 export EMAIL=kirin13@163.com
-export INGRESS=
+export INGRESS=tektonknativebeijing07-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
